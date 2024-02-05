7cf0a2909455ae01c2ff2355d447450ea9cf5f5e
mario.arista <mario.arista.dev@gmail.com>
2024-02-05 17:05:25 +0100

    snack 4 done

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,27 +1,21 @@
-/*
-Snack 4
-Inserisci un numero, se è pari stampa il numero,
-se è dispari stampa il numero successivo.
-*/
-alert("Inserisci un numero. se è pari ti stampo il numero, sennò quello successivo");
+alert("Inserisci un numero. Se è pari ti restituisco il numero, sennò quello successivo");
 
 
-let tentativi = 0;
+let continua = true;
 
-do {
+while (continua) {
 
-    let numeroInserito = prompt("Inserisci un numero. Scrivi STOP se vuoi fermare il loop!");
+    let numeroInserito = Number(prompt("Inserisci un numero. Inserisci 0 per fermare il loop!"));
 
-    const outputElement = document.getElementById("result");
-
-    if (numeroInserito % 2 == 0) {
-        alert("Hai inserito:" + numeroInserito);
+    if (numeroInserito === 0) {
+        continua = false;
+    } else if (numeroInserito % 2 == 0) {
+        alert("Il numero che hai inserito è pari: " + numeroInserito);
     } else {
-        alert("Hai inserito:" + (numeroInserito + 1));
+        alert("Il numero che hai inserito è dispari. Il numero successivo è: " + (numeroInserito + 1));
+
     }
 
-    tentativi++;
+}
 
-
-}while (richiesta === "STOP");
-
+const end = document.getElementById("result").innerHTML = "Hai voluto finire questo ciclo infinito! Non hai tutti i torti!"

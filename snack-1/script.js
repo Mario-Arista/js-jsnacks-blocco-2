27803f420b206eec6ef869cf175ad815d5359ba3
mario.arista <mario.arista.dev@gmail.com>
2024-02-05 14:58:54 +0100

    snack 1 done (with console log, not in the page)

---
+++ snack-1/script.js
@@ -0,0 +1,35 @@
+/*
+Snack 1
+Crea un array vuoto e chiedi all’utente un numero da 
+inserire nell’array. Continua a chiedere i numeri 
+all’utente e a inserirli nell’array fino a quando la 
+somma degli elementi è minore di 50
+*/
+
+// Creo array vuoto
+let arrayWithNumbers = [];
+
+// Inizializzo somma per numero dentro array a 0
+let sum = 0;
+
+let number;
+
+while (sum < 50) {
+    let number = parseInt(prompt("Inserisci un numero"));
+
+    if(isNaN(number)) {
+        alert("Il numero inserito non è valido");
+    }
+
+    arrayWithNumbers.push(number);
+
+    sum += number;
+    
+    console.log("Tentativi fatti")
+    console.log(sum);
+    console.log(arrayWithNumbers);
+
+}   
+
+
+
---
+++ snack-3/script.js
@@ -0,0 +1,8 @@
+/*
+Snack 3
+Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi all’array che ha meno elementi
+fino a quando ne avrà tanti quanti l’altro.
+(Se iniziate con un array da 3 elementi e l'altro da 5,
+eseguite istruzioni sinchè non ne hanno entrambi 5)
+/*

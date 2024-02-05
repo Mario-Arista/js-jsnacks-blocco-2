ef2140fd2c261d8957ea3822111d3c845de1210f
mario.arista <mario.arista.dev@gmail.com>
2024-02-05 16:24:24 +0100

    first attempt snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -2,4 +2,26 @@
 Snack 4
 Inserisci un numero, se è pari stampa il numero,
 se è dispari stampa il numero successivo.
-/*
+*/
+alert("Inserisci un numero. se è pari ti stampo il numero, sennò quello successivo");
+
+
+let tentativi = 0;
+
+do {
+
+    let numeroInserito = prompt("Inserisci un numero. Scrivi STOP se vuoi fermare il loop!");
+
+    const outputElement = document.getElementById("result");
+
+    if (numeroInserito % 2 == 0) {
+        alert("Hai inserito:" + numeroInserito);
+    } else {
+        alert("Hai inserito:" + (numeroInserito + 1));
+    }
+
+    tentativi++;
+
+
+}while (richiesta === "STOP");
+
